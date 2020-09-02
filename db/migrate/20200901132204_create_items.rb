c9028e1d68bd51daed66fd7d95920daac1c4b907
lion-jg <0805jgnm@gmail.com>
2020-09-02 12:45:43 +0900

    ItemsテーブルのActiveHashの作成

---
+++ db/migrate/20200901132204_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string     :image                   , null: false
+      t.string     :item_name               , null: false
+      t.text       :detail                  , null: false
+      t.integer    :category_id             , null: false
+      t.integer    :item_condition_id       , null: false
+      t.integer    :delivery_fee_id         , null: false
+      t.integer    :shipping_origin_id      , null: false
+      t.integer    :shipping_leadtime_id    , null: false
+      t.integer    :price                   , null: false
+
+      t.timestamps
+    end
+  end
+end
