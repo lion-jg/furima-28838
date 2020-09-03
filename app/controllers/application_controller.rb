0691e35305b18a3041bf6df98a1d52a2e83c0945
lion-jg <0805jgnm@gmail.com>
2020-09-03 20:51:33 +0900

    usersテーブルのキーの保存機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :family_name, :first_name, :family_name_furigana, :first_name_furigana, :birth])
+  end
+
 
   private
 
