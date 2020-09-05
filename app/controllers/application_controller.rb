d8107bcee8444d0e98bb15768f024ce54f365b43
lion-jg <0805jgnm@gmail.com>
2020-09-05 13:38:21 +0900

    usersテーブルへ保存されるカラム変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :family_name, :first_name, :family_name_furigana, :first_name_furigana, :birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_furigana, :first_name_furigana, :birth])
   end
 
 
