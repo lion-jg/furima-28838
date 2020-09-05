bca2d8b407d7cd7cdc7581524d94ac3a91f0e2f2
lion-jg <0805jgnm@gmail.com>
2020-09-06 01:01:10 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :email, :password_confirmation, :birth ,presence: true
+  validates :nickname, :email, :birth ,presence: true
 
-  validates :password,  length: { minimum: 6 }, presence: true
-  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: '' 
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :family_name
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
     validates :family_name_furigana
     validates :first_name_furigana
   end
