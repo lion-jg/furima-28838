0304c998e36044e99d0043214eac07adad35ec31
lion-jg <0805jgnm@gmail.com>
2020-09-18 02:58:26 +0900

    出品者の購入画面遷移防止

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,22 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  before_action :set_purchase, only: [:index, :sell_user]
+  before_action :sell_user, only: [:index]
 
   def index
-    # @purchase = Purchase.new
+  end
+
+  
+  private
+
+  def set_purchase
     @item = Item.find(params[:item_id])
+  end
+
+  def sell_user
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
 end
