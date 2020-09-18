0424a9a0dfefc4a9f9c285128327374ee80a499a
lion-jg <0805jgnm@gmail.com>
2020-09-18 12:32:52 +0900

    バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,19 @@
 class Address < ApplicationRecord
   belongs_to :purchase
+  before_validation :delete_hyphen
+
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
+    validates :shipping_origin_id, numericality: { other_than: 1 , message:'Select'}
+    validates :city
+    validates :block_num
+    validates :phone_num
+
+  end
+
+  private
+    def delete_hyphen
+      self.phone_num = phone_num.gsub(/-/,'')
+    end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,5 @@
 class Purchase < ApplicationRecord
+  belongs_to_active_hash :shipping_origin
   belongs_to :user
   belongs_to :item
   has_one :address
