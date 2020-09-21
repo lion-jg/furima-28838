8235ff956e499298ecb18169b80aa816b9e87f59
lion-jg <0805jgnm@gmail.com>
2020-09-21 12:07:48 +0900

    スペース入力

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,7 @@ class ItemsController < ApplicationController
   end
 
   private
-
+ 
   def item_params
     params.require(:item).permit(:image, :item_name, :detail, :category_id, :item_condition_id, :delivery_fee_id, :shipping_origin_id, :shipping_leadtime_id, :price).merge(user_id: current_user.id)
   end
